0f41d0bfe6ddfd37a855d34c84622c4300c16934
DhruvilGodhani <95485633+DhruvilGodhani@users.noreply.github.com>
2022-10-15 11:57:06 +0530

    Delete raise exceptional.py

--- raise exceptional.py
+++
@@ -1,8 +0,0 @@
-try:
-    x=int(input("Enter a age upto 18:"))
-    if x>18:
-        raise ValueError(x)
-except ValueError:
-    print(x,"is out of allowed range")
-else:
-    print(x,"is within the allowed range")
